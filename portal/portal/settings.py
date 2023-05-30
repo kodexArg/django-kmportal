70325b0d61b5af85dbf204e2bc2d6bfcbc83c69a
KodexArg <gcavedal@gmail.com>
2023-05-30 11:11:49 -0300

    Translations

--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
+    "django.contrib.sites",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
