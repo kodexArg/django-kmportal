4b5483e696e6ab330443ee91d0cd512438206b40
Kodex <gcavedal@gmail.com>
2023-06-05 00:46:35 -0300

    translation script is beautiful.. too bad I used the same file to generate de json and to read it...

--- extras/scripts/translations.py
+++ extras/scripts/translations.py
@@ -1,4 +1,3 @@
-
 import json
 import polib
 
@@ -9,13 +8,17 @@ with open("translations.json") as file:
 # Cycle through each language in the translations
 for language in translations[list(translations.keys())[0]].keys():
     # Load the .po file for this language
-    po_file_path = f'./portal/locale/{language}/LC_MESSAGES/django.po'
+    po_file_path = f"./portal/locale/{language}/LC_MESSAGES/django.po"
     po = polib.pofile(po_file_path)
 
     # Cycle through the translations in the .po file
     for entry in po:
+        print(f"checking {entry.msgid} in {language} translation")
         if entry.msgid in translations:
             entry.msgstr = translations[entry.msgid][language]
+        else:
+            print(f"no translation for {entry.msgid} in {language} translation")
+            entry.msgstr = ""
 
     # Save the modified .po file
     po.save(po_file_path)
