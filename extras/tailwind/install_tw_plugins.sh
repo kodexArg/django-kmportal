d7fce5586ed9f2cbdf8c149a90f11fd8d08ac000
Kodex <gcavedal@gmail.com>
2023-06-06 18:01:29 -0300

    Focus: Modules

---
+++ extras/tailwind/install_tw_plugins.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Add Tailwind CSS plugins
+npm install @tailwindcss/typography
+npm install @tailwindcss/forms
+npm install @tailwindcss/aspect-ratio
+npm install @tailwindcss/container-queries
