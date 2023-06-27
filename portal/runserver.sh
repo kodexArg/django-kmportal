464455dfaafd52a8753febd4b7bfdabefe3cc9b0
Kodex <gcavedal@gmail.com>
2023-06-26 21:22:57 -0300

    Github Actions

--- portal/runserver.sh
+++ portal/runserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-    python manage.py runserver 0.0.0.0:8080
+    python3 manage.py runserver 0.0.0.0:8080
 
     if [[ "$?" != "0" ]]; then
         echo "Server crashed with exit code $?.  Respawning.." >&2
