a3b30cd929faf7fab493706e12d9f4e4cc06f884
Kodex <gcavedal@gmail.com>
2023-09-08 18:49:50 -0300

    Setting up new deploy

---
+++ extras/scripts/new-ec2-required.sh
@@ -0,0 +1,17 @@
+# Update package lists
+sudo apt update
+
+# Install Python3, Pip3
+sudo apt install python3 python3-pip
+
+# Install Git
+sudo apt install git
+
+# Install Gunicorn
+pip3 install gunicorn
+
+# Install Tmux
+sudo apt install tmux
+
+# Install Fuser
+sudo apt install psmisc
